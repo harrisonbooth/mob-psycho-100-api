15b692f8e463a8c968d784d956d9ab3dbb092f71
Harrison Booth <harrisonbooth@ymail.com>
2017-07-24 16:53:20 +0100

    add html welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+
   scope path: "api" do
     resources :characters, defaults: {format: :json}
     resources :organisations, defaults: {format: :json}
